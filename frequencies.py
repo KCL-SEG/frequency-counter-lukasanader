28ce851495eeea7e69038d9e71a072eee06a5459
lukasanader <lukasanader@hotmail.com>
2023-09-26 12:31:38 +0100

    work

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        item=str(item)
+        if item in frequencies:
+            frequencies[item]=frequencies.get(item)+1
+        else:
+            frequencies[item]=1
     return frequencies
+
